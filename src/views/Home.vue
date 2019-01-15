b0ff4814af64a84608083e92619bcf93c2f8e236
zhaoqi <mezhaoqi@163.com>
2019-01-15 19:58:51 +0800

    首页侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
+  <div class="home">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+          <div class="logo"></div>
+        <el-menu
+          default-active="2"
+          class="el-menu-admin"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-container>
         <el-header>Header</el-header>
         <el-main>Main</el-main>
@@ -17,43 +39,61 @@ export default {
     getUserList(params).then(res => {
       console.log(res);
     });
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
-.el-header,
-.el-footer {
-  background-color: #545c64;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-aside {
-  background-color: #545c64;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
+.home {
+  height: 100%;
+  background-color: #e5e5e5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #009688;
+  }
+  .logo {
+    height: 60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: white;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: white;
+    cursor: pointer;
+    &:hover {
+      background-color: #00635a;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
 }
 </style>
 
